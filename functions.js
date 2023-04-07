f46dd44bdd90d4f935cab7fd3a45e7d05c86c83f
Fabi Tureo <fabianestebantureo@gmail.com>
2023-04-07 18:32:10 -0300

    👾 Add examples for practice with functions

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-console.log("Functions Working")
+console.log("Functions Working");
 
 // Una función es un bloque de código que realiza una tarea específica. Puede tomar entradas (argumentos-parámetros) y devolver salidas, y puede llamarse o invocarse en cualquier punto de un programa (¿Se puede?).
 
@@ -13,7 +13,7 @@ console.log("Functions Working")
 // Ejemplo utilizando la palabra function
 // Defino qué va a hacer esa función: Imprimir un mensaje en consola que "Hello World from a function"
 
-// Regular Function 
+// Regular Function
 // function nameFunction(parameter1, parameter2, ..., parameterN){
 //     // Block of code to execute
 // }
@@ -37,27 +37,26 @@ console.log("Functions Working")
 // }
 // printByeWorld()
 
-// Hacer el código de arriba no sería una buena práctica. 
+// Hacer el código de arriba no sería una buena práctica.
 // Entonces hacemos una función que reciba por parámetro el texto a mostrar, de esta forma no tengo que escribir una para Hello y otra para Bye, simplemente invoco a la función pasándole por parámetro el mensaje que quiero
 
-// Regular Function 
+// Regular Function
 
 function printInConsole(message) {
-    console.log(message)
+    console.log(message);
 }
 
 // Quiero ver el hello, entonces como parámetro le paso el mensaje que quiero
-printInConsole("Hello World from a function")
+printInConsole("Hello World from a function");
 // Quiero ver el bye, entonces como parámetro le paso el mensaje que quiero
-printInConsole("Bye World from a function")
+printInConsole("Bye World from a function");
 // Quiero ver el mensaje "Aguante la 38i"
-printInConsole("Aguante la 38i")
-
+printInConsole("Aguante la 38i");
 
 // Arrow Function
 // Definir el tipo de variable, recomendado como siempre: const
-// Como a todas las variables de tipo const, la declaramos, le asignamos un nombre y la inicializamos. 
-// La flecha refiere al símbolo que se crea utilizando un "=" seguido de un bracket ">", conformando => 
+// Como a todas las variables de tipo const, la declaramos, le asignamos un nombre y la inicializamos.
+// La flecha refiere al símbolo que se crea utilizando un "=" seguido de un bracket ">", conformando =>
 // const nameFunction = (parameter1, parameter2, ..., parameterN) => {
 //     // Block of code to execute
 //     // Los paréntesis no siempre son necesarios en los parametros y las llaves no siempre son necesarias para el    bloque de código a ejecutar
@@ -66,16 +65,15 @@ printInConsole("Aguante la 38i")
 
 const printInConsoleArrowFunction = (message) => console.log(message);
 
-printInConsoleArrowFunction("Vamos a renderizar este mensaje")
-printInConsoleArrowFunction("Aguante la 38i desde Arrow Function")
+printInConsoleArrowFunction("Vamos a renderizar este mensaje");
+printInConsoleArrowFunction("Aguante la 38i desde Arrow Function");
 
-const suma = 2 + 3 
-console.log(suma)
-const sum1 = 3 + 5
-console.log(sum1)
-const sum2 = 4 + 6
-console.log(sum2)
-
+const suma = 2 + 3;
+console.log(suma);
+const sum1 = 3 + 5;
+console.log(sum1);
+const sum2 = 4 + 6;
+console.log(sum2);
 
 // Voy a intentar encontrar una forma más eficiente de resolver cualquier suma que me pidan
 // Voy a usar una funcion
@@ -94,7 +92,6 @@ console.log(sum2)
 // 2.3 Asegurarme de que entre ellos dos exista el signo "+" para realizar la realizar la operación aritmética
 // 2.4 Realizar la operación aritmética
 
-
 // 3. ¿Cómo hace nuestra función para resolver la suma?
 // 3.1 Nombrar "n1" al primer parámetro y deben ser de tipo numérico
 // 3.2 Nombrar "n2" al segundo parámetro y deben ser de tipo numérico
@@ -105,13 +102,11 @@ console.log(sum2)
 // 4.1 Invoco a la función llamándola por su nombre de referencia seguido de ()
 // 4.2 Le paso DOS parámetros asegurándome de que sean números. Luego los validaremos en la función (MVP2)
 
-
-// // Opción #1 - Escribiendo el console.log como parte del statement de la función. 
+// // Opción #1 - Escribiendo el console.log como parte del statement de la función.
 // function sum(n1, n2){
 //     console.log(n1 + n2)
 // }
 // sum(1, 3);
-
 
 // // Opción #2 - Escribiendo el primer retorno
 // // Pero si llamo a console.log(sum(n1, n2) me tira undefined)
@@ -164,7 +159,7 @@ console.log(sum2)
 
 // // Tarea Division 1.1: Redondear hacia arriba
 // // Tarea Division 1.2: Redondear hacia abajo
-// // Tarea Division 1.3: Truncar en la cantidad solicitada 
+// // Tarea Division 1.3: Truncar en la cantidad solicitada
 
 // console.log("División: ", divide(354, 547)) // Output: 0.6471663619744058
 
@@ -180,11 +175,10 @@ console.log(sum2)
 
 // // Tarea Division 2: Validar que el segundo parámetro sea distinto de 0
 
-const sum = (a, b) => a + b
-const substract = (a, b) => a - b
-const multiply = (a, b) => a * b
-const divide = (a, b) => a / b // Recordar sobre el valor 
-
+const sum = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b; // Recordar sobre el valor
 
 function calculateOperation(a, b, operation) {
     switch (operation) {
@@ -196,6 +190,7 @@ function calculateOperation(a, b, operation) {
             return a * b;
         case "/":
             return a / b;
+        // Pending: Validation for number zero
         case "^":
             return Math.pow(a, b);
         default:
@@ -206,9 +201,9 @@ function calculateOperation(a, b, operation) {
 // Repasar lo que es una "Potencia" en matemática
 // 2^3 = 2 * 2 * 2 - 8 - En este caso el 2 representa a la base y el 3 representa a la potencia.
 // 3^2 = 3 * 3 - 9
-// 4^5 = 4 * 4 * 4 * 4 * 4 - 
+// 4^5 = 4 * 4 * 4 * 4 * 4 -
 
-console.log("Suma en calculateOperation", calculateOperation(2, 3, "+"))
+console.log("Suma en calculateOperation", calculateOperation(2, 3, "+"));
 // a = 2
 // b = 3
 // operation = "+"
@@ -218,7 +213,10 @@ console.log("Resta en calculateOperation", calculateOperation(2, 3, "-"));
 // b = 3
 // operation = "-"
 
-console.log("Multiplicación en calculateOperation", calculateOperation(2, 3, "*"));
+console.log(
+    "Multiplicación en calculateOperation",
+    calculateOperation(2, 3, "*")
+);
 // a = 2
 // b = 3
 // operation = "*"
@@ -232,3 +230,96 @@ console.log("Potencia en calculateOperation", calculateOperation(2, 3, "^"));
 // a = 2
 // b = 3
 // operation = "^"
+
+const calculate = (a, b, operation) => {
+    console.log(a, b, operation);
+    const operations = {
+        "+": () => a + b,
+        "-": () => a - b,
+        "*": () => a * b,
+        "/": () => a / b,
+        "": () => Math.pow(a, b),
+    };
+    // operations[operation]; // output: +, -, *, /, ^
+    const operacionFunc = operations[operation];
+
+    // operations[operation] con operation = "+" // output: () => a + b
+    // operations[operation] con operation = "-" // output: () => a - b
+    // operations[operation] con operation = "*" // output: () => a * b
+    // operations[operation] con operation = "/" // output: () => a / b
+    // operations[operation] con operation = "^" // output: () => Math.pow(a, b)
+    // operations[operation] con operation = "}" // output: undefined
+
+    if (operacionFunc) {
+        return operacionFunc(a, b);
+    } else {
+        return "Invalid operation";
+    }
+};
+
+console.log("Suma en calculate", calculate(16, 3, "+"));
+console.log("Resta en calculate", calculate(16, 3, "-"));
+// Tarea: Probar con más ejemplos y si quieren, sumar más operaciones
+
+// Template Strings: Se usa entre backticks y las variables se colocan entre ${}
+const getLengthString = (string) => {
+    return `El mensaje colocado posee una longitud de ${string.length} caracteres`;
+};
+
+console.log(getLengthString("gato"));
+console.log(getLengthString("perro"));
+console.log(getLengthString("gato y perro"));
+
+function calculateAverage(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    return total / numbers.length;
+}
+
+const numbersToAverage = [2, 4, 6, 8];
+console.log(calculateAverage(numbersToAverage));
+
+function capitalizeFirstLetter(string) {
+    // return string.charAt(0).toUpperCase() + string.slice(1);
+    // Selecciono la primera letra
+    const firstLetter = string.charAt(0)
+    console.log(firstLetter)
+    // La convierto a mayúscula
+    const firstLetterUpperCase = firstLetter.toUpperCase()
+    console.log(firstLetterUpperCase)
+    // Selecciono el resto de la palabra
+    const restOfTheWord = string.slice(1)
+    console.log(restOfTheWord)
+    // Concateno la primera letra en mayúscula con el resto de la palabra
+    return firstLetterUpperCase + restOfTheWord
+}
+
+// Example usage:
+console.log(capitalizeFirstLetter("hello")); // Output: "Hello"
+console.log(capitalizeFirstLetter("aguante la 38i"));
+
+const textToTransform = "    ¡Comisión 38i La Mejor!    ";
+// Output: comision-38i-la-mejor
+
+const transformTextToSlug = (text) => {
+    // text: ¡Comisión 38i La Mejor!
+    return (
+        text
+            .normalize("NFD")
+            //    ¡Comisio´n 38i La Mejor!
+            .toLowerCase()
+            //    ¡comisio´n 38i la mejor!
+            .trim()
+            // ¡comisio´n 38i la mejor!
+            .replace(/\s+/g, " ") // Esta RegEx se encarga de reemplazar los espacios intermedios entre palabra y palabra por lo que pasemos en el segundo parámetro, en este caso lo reemplaza con un espacio
+            // ¡comisio´n 38i la mejor!
+            .replace(/[^\w ]+/g, "")
+            // quita los caracteres especiales
+            .replace(/\s+/g, "-")
+            // reemplaaza los espacios con un guion medio
+    );
+};
+
+console.log(transformTextToSlug(textToTransform));
