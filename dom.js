39dbbb54f8fcbfe0ccd47c2ee072093fc69e123c
Fabi Tureo <fabianestebantureo@gmail.com>
2023-04-10 21:43:33 -0300

    👾 Beginning with DOM and dynamic render

---
+++ dom.js
@@ -0,0 +1,11 @@
+console.log("DOM working")
+console.log("a ver que muestra 'document'", document)
+
+const firstH1 = document.getElementById("first-h1");
+console.log("first element in DOM", firstH1)
+
+const secondH1 = document.getElementById("second-h1")
+console.log("second element in DOM", secondH1.textContent)
+
+
+// Pending: Add dynamic content with JS
